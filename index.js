f0c5884f8c7442320738beb228d7b46ed9257cd7
Lucas Rennó Kallás <lucasrenn@hotmail.com>
2023-02-03 02:06:46 -0300

    feat: send email with html data

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const generateHtml = require("./generateHtml/generateHtml");
+const nodemailer = require("nodemailer");
 
 const axios = require("axios");
 require("dotenv").config();
@@ -8,9 +9,39 @@ const today = new Date().toISOString().split("T")[0];
 const baseUrl = `https://newsapi.org/v2/top-headlines?country=br&from=${today}&apiKey=`;
 const apiKey = process.env.API_KEY;
 
+const sendMail = (htmlEmail) => {
+	const transporter = nodemailer.createTransport({
+		host: "smtp.gmail.com", // We also used gmail smtp
+		port: 587,
+		secure: false,
+		auth: {
+			user: process.env.EMAIL_USER,
+			pass: process.env.EMAIL_PASS,
+		},
+		tls: { rejectUnauthorized: false },
+	});
+
+	const mailOptions = {
+		from: process.env.EMAIL_USER,
+		to: process.env.TO_EMAIL,
+		subject: `🆕 Notícias Diárias ${new Date().toLocaleDateString()} | NO-REPLY`,
+		html: htmlEmail,
+	};
+
+	transporter.sendMail(mailOptions, function (error, info) {
+		if (error) {
+			console.log(error);
+		} else {
+			console.log("Email sent | response: " + info.response);
+		}
+	});
+};
+
 axios.get(baseUrl + apiKey).then((response) => {
 	if (response.data.articles) {
 		const email = generateHtml(response.data.articles);
-		console.log(email.join(""));
+		const htmlEmailData = email.join("");
+
+		sendMail(htmlEmailData);
 	}
 });
