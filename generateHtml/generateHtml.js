da377f2476246d1def9c83949b4bbc7d8c4ece85
Lucas Rennó Kallás <lucasrenn@hotmail.com>
2023-02-07 17:49:31 -0300

    feat: added limit and new api request (nyt news)

--- generateHtml/generateHtml.js
+++ generateHtml/generateHtml.js
@@ -1,9 +1,12 @@
 const newsTemplate = require("./templates/newsTemplate");
 
-const generateHtml = (articles) => {
+const generateHtml = (articles, limit = 9999) => {
 	const emailData = [];
 
 	for (let i = 0; i < articles.length; i++) {
+		if (i >= limit) {
+			break;
+		}
 		emailData.push(newsTemplate(articles[i]));
 	}
 
--- index.js
+++ index.js
@@ -36,16 +36,36 @@ const sendMail = (htmlEmail) => {
 
 // This job runs at 9:00AM
 const job = schedule.scheduleJob({ hour: 9, minute: 0 }, () => {
+	let htmlEmailData = "";
 	const today = new Date().toISOString().split("T")[0];
+	const limitSetToSources = 5;
+	const timeoutInSecs = 10;
+
 	const baseUrl = `https://newsapi.org/v2/top-headlines?country=br&from=${today}&apiKey=`;
+	const baseUrlUs = `https://newsapi.org/v2/top-headlines?country=us&from=${today}&apiKey=`;
+	const newYorkTimesBaseUrl = `https://api.nytimes.com/svc/news/v3/content/nyt/all.json?api-key=`;
+
+	axios.get(newYorkTimesBaseUrl + process.env.API_KEY_NYT).then((response) => {
+		if (response.data.results) {
+			htmlEmailData += generateHtml(response.data.results, limitSetToSources);
+		}
+	});
+
+	axios.get(baseUrlUs + process.env.API_KEY).then((response) => {
+		if (response.data.articles) {
+			htmlEmailData += generateHtml(response.data.articles, limitSetToSources);
+		}
+	});
 
 	axios.get(baseUrl + process.env.API_KEY).then((response) => {
 		if (response.data.articles) {
-			const htmlEmailData = generateHtml(response.data.articles);
-
-			sendMail(htmlEmailData);
+			htmlEmailData += generateHtml(response.data.articles, limitSetToSources);
 		}
 	});
+
+	setTimeout(() => {
+		sendMail(htmlEmailData);
+	}, timeoutInSecs * 1000);
 });
 
 job.schedule();
