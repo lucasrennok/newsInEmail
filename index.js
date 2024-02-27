7201be02a61bf437e81b2f2c28bd36b9a86660cc
Lucas Rennó Kallás <lucasrenn@hotmail.com>
2024-02-27 18:29:39 -0300

    feat: schedule time moved to env vars]

--- index.js
+++ index.js
@@ -35,37 +35,51 @@ const sendMail = (htmlEmail) => {
 };
 
 // This job runs at 9:00AM
-const job = schedule.scheduleJob({ hour: 9, minute: 0 }, () => {
-	let htmlEmailData = "";
-	const today = new Date().toISOString().split("T")[0];
-	const limitSetToSources = 5;
-	const timeoutInSecs = 10;
+const job = schedule.scheduleJob(
+	{ hour: process.env.SEND_HOUR, minute: process.env.SEND_MINUTE },
+	() => {
+		let htmlEmailData = "";
+		const today = new Date().toISOString().split("T")[0];
+		const limitSetToSources = 5;
+		const timeoutInSecs = 10;
 
-	const baseUrl = `https://newsapi.org/v2/top-headlines?country=br&from=${today}&apiKey=`;
-	const baseUrlUs = `https://newsapi.org/v2/top-headlines?country=us&from=${today}&apiKey=`;
-	const newYorkTimesBaseUrl = `https://api.nytimes.com/svc/news/v3/content/nyt/all.json?api-key=`;
+		const baseUrl = `https://newsapi.org/v2/top-headlines?country=br&from=${today}&apiKey=`;
+		const baseUrlUs = `https://newsapi.org/v2/top-headlines?country=us&from=${today}&apiKey=`;
+		const newYorkTimesBaseUrl = `https://api.nytimes.com/svc/news/v3/content/nyt/all.json?api-key=`;
 
-	axios.get(newYorkTimesBaseUrl + process.env.API_KEY_NYT).then((response) => {
-		if (response.data.results) {
-			htmlEmailData += generateHtml(response.data.results, limitSetToSources);
-		}
-	});
+		axios
+			.get(newYorkTimesBaseUrl + process.env.API_KEY_NYT)
+			.then((response) => {
+				if (response.data.results) {
+					htmlEmailData += generateHtml(
+						response.data.results,
+						limitSetToSources
+					);
+				}
+			});
 
-	axios.get(baseUrlUs + process.env.API_KEY).then((response) => {
-		if (response.data.articles) {
-			htmlEmailData += generateHtml(response.data.articles, limitSetToSources);
-		}
-	});
+		axios.get(baseUrlUs + process.env.API_KEY).then((response) => {
+			if (response.data.articles) {
+				htmlEmailData += generateHtml(
+					response.data.articles,
+					limitSetToSources
+				);
+			}
+		});
 
-	axios.get(baseUrl + process.env.API_KEY).then((response) => {
-		if (response.data.articles) {
-			htmlEmailData += generateHtml(response.data.articles, limitSetToSources);
-		}
-	});
+		axios.get(baseUrl + process.env.API_KEY).then((response) => {
+			if (response.data.articles) {
+				htmlEmailData += generateHtml(
+					response.data.articles,
+					limitSetToSources
+				);
+			}
+		});
 
-	setTimeout(() => {
-		sendMail(htmlEmailData);
-	}, timeoutInSecs * 1000);
-});
+		setTimeout(() => {
+			sendMail(htmlEmailData);
+		}, timeoutInSecs * 1000);
+	}
+);
 
 job.schedule();
